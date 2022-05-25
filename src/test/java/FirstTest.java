fff0753af4e4d225802dc6f59f6c63f88465bd28
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-05-25 12:36:44 +0300

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+
+public class FirstTest extends TestBase {
+  @Test
+
+  public void myFirstTest() {
+    driver.navigate().to("https://yandex.ru/");
+    driver.findElement(By.id("text")).sendKeys("maven");
+    driver.findElement(By.cssSelector(".search2")).submit();
+    wait.until(titleContains("maven — Яндекс"));
+  }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+  public WebDriver driver;
+  public WebDriverWait wait;
+
+  @BeforeSuite
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+  }
+
+  @AfterSuite
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+}
