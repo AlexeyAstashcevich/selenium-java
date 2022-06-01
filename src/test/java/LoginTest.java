74489aea861c0f775f85507aacfe5048b1b4cf23
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-06-01 12:47:55 +0300

    Создан тест на проверку заголовков в меню администратора

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,18 +1,30 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class LoginTest extends TestBase {
   @Test
-  public void loginTest(){
+  public void loginTest() {
     navigation.gotTo("http://localhost/litecart/admin/");
-    navigation.type(By.name("username"),"admin");
-    navigation.type(By.name("password"),"admin");
+    navigation.type(By.name("username"), "admin");
+    navigation.type(By.name("password"), "admin");
     navigation.click(By.name("login"));
     wait.until(titleIs("My Store"));
-    navigation.click(By.name("Appearence"));
-    Assert.assertTrue(helperBase.isElementPresent(driver, By.cssSelector("h1")));
+    for (int i = 1; i < 18; i++) {
+      String path = "//li[@id='app-'][" + i + "]";
+      navigation.click(By.xpath(path));
+      Assert.assertTrue(helperBase.isElementPresent(driver, By.cssSelector("h1")));
+      if (helperBase.isElementPresent(driver, By.xpath(path + "/ul[@class='docs']"))) {
+        List<WebElement> elements = driver.findElements(By.xpath(path + "/ul[@class='docs']/li"));
+        for (int l = 1; l < elements.size(); l++)
+          navigation.click(By.xpath(path + "/ul[@class='docs']/li[" + l + "]/a"));
+        Assert.assertTrue(helperBase.isElementPresent(driver, By.cssSelector("h1")));
+      }
+    }
   }
 }
