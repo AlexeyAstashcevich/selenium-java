ed8efd732e42e48c365b808336bc20ac65f59818
simpotka76 <zbvnr@yandex.ru>
2022-06-09 19:40:09 +0300

    Создан тест для проверки последовательности стран

--- src/test/java/LettersSortTest.java
+++ src/test/java/LettersSortTest.java
@@ -1,6 +1,5 @@
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,37 +10,37 @@ import java.util.List;
 
 
 public class LettersSortTest extends TestBase {
-  @Test
-  public void sortTest() {
-    login();
-    navigation.gotTo("http://localhost/litecart/admin/?app=countries&doc=countries");
-    List<WebElement> countries = driver.findElements(By.xpath("//*[@class='row']/td[5]/a"));
-    List<String> countryList = new ArrayList<>();
-    for (WebElement wec : countries) {
-      countryList.add(wec.getText());
-      ArrayList<String> countrySorted = (ArrayList<String>) countryList;
-      Collections.sort(countrySorted);
-      Assert.assertEquals(countrySorted, countryList);
-    }
-    for (WebElement wek: countries){
-        WebElement countriesInside = wek.findElement(By.xpath("..//..//td[6]"));
-        if (!countriesInside.getText().equals("0")) {
-          driver.findElement(By.cssSelector("Body")).sendKeys(Keys.CONTROL + "t");
-          driver.navigate().to(countriesInside.getAttribute("baseURI"));
-          List<String> insideCountry = new ArrayList<>();
-          List<WebElement> list = driver.findElements(By.xpath("*//tr/td[3]/input[@type='hidden']"));
-          for (WebElement ins:list){
-            insideCountry.add( ins.getAttribute("defaultValue"));
-          }
-          ArrayList<String> insideCountrySorted = (ArrayList<String>) insideCountry;
-          Collections.sort(insideCountrySorted);
-          Assert.assertEquals(insideCountry,insideCountrySorted );
-//          list.removeAll(list);
-//          insideCountry.removeAll(insideCountry);
-//          insideCountrySorted.removeAll(insideCountrySorted);
-          driver.navigate().back();
+    @Test
+    public void sortTest() {
+        login();
+        navigation.gotTo("http://localhost/litecart/admin/?app=countries&doc=countries");
+        List<WebElement> countries = driver.findElements(By.xpath("//*[@class='row']/td[5]/a"));
+        List<String> countryList = new ArrayList<>();
+        for (WebElement wec : countries) {
+            countryList.add(wec.getText());
+            ArrayList<String> countrySorted = (ArrayList<String>) countryList;
+            Collections.sort(countrySorted);
+            Assert.assertEquals(countrySorted, countryList);
         }
+        List<String> insideCountryList = new ArrayList<>();
+        for (WebElement wek : countries) {
+            WebElement countriesInside = wek.findElement(By.xpath("..//..//td[6]"));
+            if (!countriesInside.getText().equals("0")) {
 
-      }
-  }
+                insideCountryList.add(wek.findElement(By.xpath("..//..//td[5]/a")).getAttribute("href"));
+            }
+        }
+        for (String url : insideCountryList) {
+            driver.get(url);
+            List<String> insideCountry = new ArrayList<>();
+            List<WebElement> list = driver.findElements(By.xpath("*//tr/td[3]/input[@type='hidden']"));
+            for (WebElement ins : list) {
+                insideCountry.add(ins.getAttribute("defaultValue"));
+            }
+            ArrayList<String> insideCountrySorted = (ArrayList<String>) insideCountry;
+            Collections.sort(insideCountrySorted);
+            Assert.assertEquals(insideCountry, insideCountrySorted);
+        }
+    }
 }
+
