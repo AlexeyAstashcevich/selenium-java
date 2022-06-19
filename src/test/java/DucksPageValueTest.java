2a989f2985bccfed3801a78b7571a79b454730fc
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-06-19 16:15:35 +0300

    Update DucksPageValueTest.java

--- src/test/java/DucksPageValueTest.java
+++ src/test/java/DucksPageValueTest.java
@@ -1,9 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class DucksPageValueTest extends TestBase{
+public class DucksPageValueTest extends TestBase {
   @Test
-  public void duckPagesValue(){
+  public void duckPagesValue() {
     navigation.gotTo("http://localhost/litecart/");
+    WebElement duckLink = driver.findElements(By.cssSelector("div#box-campaigns a.link")).iterator().next();
+    String duckTitle = duckLink.findElement(By.className("name")).getText();
+    String duckPrice = duckLink.findElement(By.className("regular-price")).getText();
+    String color = duckLink.findElement(By.className("manufacturer")).getCssValue("color");
+    String[] charsList = color.split(",");
+
+    Assert.assertEquals();
+
 
   }
 }
