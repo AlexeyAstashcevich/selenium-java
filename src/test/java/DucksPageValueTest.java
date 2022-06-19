7388e89bbe4a8aeca60315aea55a7b161f4f0095
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-06-19 14:33:51 +0300

    Создан тест для проверки геозон.

---
+++ src/test/java/DucksPageValueTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class DucksPageValueTest extends TestBase{
+  @Test
+  public void duckPagesValue(){
+    navigation.gotTo("http://localhost/litecart/");
+
+  }
+}
--- src/test/java/GeozonesTest.java
+++ src/test/java/GeozonesTest.java
@@ -11,43 +11,32 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
-public class GeozonesTest extends TestBase{
+public class GeozonesTest extends TestBase {
     @Test
 
     public void geoTest() throws InterruptedException {
         admin.login();
         navigation.gotTo("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
         List<String> countries = driver.findElements(By.xpath("//*[@class='row']/td[3]/a"))
-                .stream().map(x->x.getAttribute("href")).collect(Collectors.toList());
+                .stream().map(x -> x.getAttribute("href")).collect(Collectors.toList());
         String mainWindow = driver.getWindowHandle();
         Set<String> oldWindows = driver.getWindowHandles();
-        for (String url: countries){
+        for (String url : countries) {
             js.executeScript("window.open();");
             String newWindow = wait.until(anyWindowOtherThan(oldWindows));
             driver.switchTo().window(newWindow);
             navigation.gotTo(url);
-            navigation.click(By.className("select2-selection__rendered"));
-            List<String> listOfCountry = driver.findElements(By.cssSelector("li.select2-results__option[id]"))
-                    .stream().map(x->x.getText()).collect(Collectors.toList());
-            List<String> sortedListOfCountry = new ArrayList<>(listOfCountry);
-            Collections.sort(sortedListOfCountry);
-            Assert.assertEquals(sortedListOfCountry, listOfCountry);
-            for (String country: listOfCountry){
-                navigation.type(By.cssSelector("input.select2-search__field"), country+ Keys.ENTER);
-                List<WebElement> zones = driver.findElements(By.cssSelector("select[name='zones[1][zone_code]']>option"));
-                navigation.click(By.className("select2-selection__rendered"));
-                if(zones.size()>1){
-                    List<String> zonesString = zones.stream().map(WebElement::getText).collect(Collectors.toList());
-                    List<String> sortedZones= new ArrayList<>(zonesString);
-                    Collections.sort(sortedZones);
-                    Assert.assertEquals(zonesString, sortedZones);
-                }
-            }
-            driver.close();
-            driver.switchTo().window(mainWindow);
-        }
+            List<String> zone = driver.findElements(By.cssSelector("td>select[name*='[zone_code]'] option[selected='selected']"))
+                    .stream().map(WebElement::getText).collect(Collectors.toList());
+           List<String> sortedZone= new ArrayList<>(zone);
+           Collections.sort(sortedZone);
+           Assert.assertEquals(zone, sortedZone);
+           driver.close();
+           driver.switchTo().window(mainWindow);
         }
     }
+
+}
+
 
