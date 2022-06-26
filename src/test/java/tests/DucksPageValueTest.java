8a2cb972c5a0c9b132a17dd809f6db8b88433e4f
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-06-26 20:20:41 +0300

    Update DucksPageValueTest.java

--- src/test/java/tests/DucksPageValueTest.java
+++ src/test/java/tests/DucksPageValueTest.java
@@ -6,6 +6,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.Color;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
 
 public class DucksPageValueTest extends TestBase {
@@ -16,6 +19,15 @@ public class DucksPageValueTest extends TestBase {
     String duckTitle = duckLink.findElement(By.className("name")).getText();
     String duckPrice = duckLink.findElement(By.className("regular-price")).getText();
     String color = duckLink.findElement(By.className("manufacturer")).getCssValue("color");
+    Color.fromString(color).getColor();
+    java.awt.Color rgb = Color.fromString(color).getColor();
+    Assert.assertTrue(rgb.getBlue() == rgb.getGreen());
+    Assert.assertTrue(rgb.getBlue() == rgb.getGreen());
+    List<String> colorOfPrice = driver.findElements(By.cssSelector("strong.campaign-price"))
+            .stream().map(x->x.getAttribute("color")).collect(Collectors.toList());
+    for(String duck: colorOfPrice){
+      Assert.assertTrue();
+    }
 
 
 //    Assert.assertEquals();
