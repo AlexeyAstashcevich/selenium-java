5fbd98d4e08b15112cffa7683c6336a4ff610158
simpotka76 <96411523+simpotka76@users.noreply.github.com>
2022-06-27 13:27:15 +0300

    создан тест для проверки страниц Уток

--- src/test/java/tests/DucksPageValueTest.java
+++ src/test/java/tests/DucksPageValueTest.java
@@ -2,36 +2,44 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.openqa.selenium.support.Color;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
 
 public class DucksPageValueTest extends TestBase {
   @Test
   public void duckPagesValue() {
     navigation.gotTo("http://localhost/litecart/");
     WebElement duckLink = driver.findElements(By.cssSelector("div#box-campaigns a.link")).iterator().next();
-    String duckTitle = duckLink.findElement(By.className("name")).getText();
+    String duckTitle = duckLink.findElement(By.cssSelector(" div.name")).getText();
     String duckPrice = duckLink.findElement(By.className("regular-price")).getText();
     String color = duckLink.findElement(By.className("manufacturer")).getCssValue("color");
-    Color.fromString(color).getColor();
     java.awt.Color rgb = Color.fromString(color).getColor();
     Assert.assertTrue(rgb.getBlue() == rgb.getGreen());
     Assert.assertTrue(rgb.getBlue() == rgb.getGreen());
-    List<String> colorOfPrice = driver.findElements(By.cssSelector("strong.campaign-price"))
-            .stream().map(x->x.getAttribute("color")).collect(Collectors.toList());
-    for(String duck: colorOfPrice){
-      Assert.assertTrue();
-    }
-
-
-//    Assert.assertEquals();
-
-
+    String priceColor = duckLink.findElement(By.cssSelector("strong.campaign-price"))
+            .getCssValue("color");
+    java.awt.Color priceRgb = Color.fromString(priceColor).getColor();
+    Assert.assertEquals(priceRgb.getBlue(), 0);
+    Assert.assertEquals(priceRgb.getGreen(), 0);
+    double sizerCampaignPrice = Double.parseDouble(duckLink.findElement(By.className("campaign-price"))
+            .getCssValue("font-size").replaceAll("px", ""));
+    double sizeRegularPrice = Double.parseDouble(duckLink.findElement(By.className("regular-price"))
+            .getCssValue("font-size").replaceAll("px", ""));
+    Assert.assertTrue(sizerCampaignPrice > sizeRegularPrice);
+    duckLink.click();
+    String duckTitleInsidePage = driver.findElement(By.cssSelector("h1.title")).getText();
+    String duckPriceInsidePage = driver.findElement(By.className("regular-price")).getText();
+    Assert.assertEquals(duckPriceInsidePage, duckPrice);
+    Assert.assertEquals(duckTitleInsidePage, duckTitle);
+    String colorInsidePage = driver.findElement(By.className("manufacturer")).getCssValue("color");
+    java.awt.Color rgbInsidePage = Color.fromString(colorInsidePage).getColor();
+    Assert.assertTrue(rgbInsidePage.getBlue() == rgbInsidePage.getGreen());
+    Assert.assertTrue(rgbInsidePage.getBlue() == rgbInsidePage.getGreen());
+    double sizerCampaignPriceInsidePage = Double.parseDouble(driver.findElement(By.className("campaign-price"))
+            .getCssValue("font-size").replaceAll("px", ""));
+    double sizeRegularPriceInsidePage = Double.parseDouble(driver.findElement(By.className("regular-price"))
+            .getCssValue("font-size").replaceAll("px", ""));
+    Assert.assertTrue(sizerCampaignPriceInsidePage > sizeRegularPriceInsidePage);
   }
 }
